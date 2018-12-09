97810033b81a74e0fd7760ac8b1ea0c3bebe7cc7
AuroraBTH <nhdandersson@gmail.com>
2018-12-09 12:08:02 +0100

    Added: author id and amount of downloads

--- main.py
+++ main.py
@@ -3,23 +3,25 @@ from requests import get
 import json
 
 def get_amount_of_pages(minecraft_version):
-    initial_site_response = get(
-        "https://www.curseforge.com/minecraft/mc-mods?filter-game-version=" + minecraft_version + "&filter-sort=5&")
+    initial_site_response = get("https://www.curseforge.com/minecraft/mc-mods?filter-game-version=" + minecraft_version + "&filter-sort=5&")
 
     soup = BeautifulSoup(initial_site_response.text, "html.parser")
     amount_of_pages = soup.find('li', class_="dots").find_next_sibling().text
 
     return int(amount_of_pages)
 
-def get_project_id(mod):
+def get_project_and_author_id(mod):
     mod_id = mod.find("a", class_="button--download").get("data-nurture-data")
+    author_id = mod.find("a", class_="button--download").get("data-nurture-data")
 
     if mod_id is None:
         mod_id = json.loads(mod.find("a", class_="button--download").get("data-exp-nurture"))["ProjectID"]
+        author_id = json.loads(mod.find("a", class_="button--download").get("data-exp-nurture"))["AuthorID"]
     else:
         mod_id = json.loads(mod_id)["ProjectID"]
+        author_id = json.loads(author_id)["AuthorID"]
 
-    return mod_id
+    return [mod_id, author_id]
 
 def write_mods_to_json(minecraft_version, file_name):
     domain = "https://www.curseforge.com"
@@ -37,17 +39,22 @@ def write_mods_to_json(minecraft_version, file_name):
         list_of_mods = data.find_all("li", class_="project-list-item")
 
         for mod in list_of_mods:
+            id_list = get_project_and_author_id(mod)
             project_name = mod.find("h2", class_="list-item__title").text.strip()
-            project_id = get_project_id(mod)
+            project_id = id_list[0]
+            project_author_id = id_list[1]
             project_category = mod.find("a", class_="category__item")["title"].strip()
             project_description = mod.find("div", class_="list-item__description").p.text.strip()
+            project_downloads = int(mod.find("span", class_="count--download").text.strip().replace(",", ""))
             project_link = mod.find("div", class_="list-item__details").a["href"]
 
             mod_data = {}
             mod_data["id"] = project_id
             mod_data["name"] = project_name
+            mod_data["author_id"] = project_author_id
             mod_data["category"] = project_category
             mod_data["description"] = project_description
+            mod_data["downloads"] = project_downloads
             mod_data["link"] = domain + project_link
 
             mod_list.append(mod_data)
